6659c496a1e10330f362c549238f1f6d05cbc401
isansa <minuent@gmail.com>
2023-08-16 23:36:42 +0700

    not finish
    first

---
+++ script.js
@@ -0,0 +1,7 @@
+var = dayNumber["sun","mon","tue","wed","thu","fri","sat"];
+
+if (dayNumber > 6 || dayNumber < 0) {
+    console.log("Invalid day number");
+} else (dayNumber == 0){
+    console.log(dayNumber[0]);
+}
---
+++ switchcode.js
@@ -0,0 +1,31 @@
+let dayNumber = ["sun","mon","tue","wed","thu","fri","sat"];
+switch (dayNumber){
+    //if day = 0
+    case dayNumber[0];
+        console.log("Sunday");
+        break;
+    //if day = 1
+    case dayNumber[1];
+        console.log("Monday");
+        break;
+    // 2
+    case dayNumber[2];
+        console.log("Tuesday");
+        break;
+    //if day = 3
+    case dayNumber[3];
+        console.log("Wednesday");
+        break;
+    //if day = 4
+    case dayNumber[4];
+        console.log("Thursday");
+        break;
+    //if day = 5
+    case dayNumber[5];
+        console.log("Friday");
+        break;
+    //if day = 6
+    case dayNumber[6];
+        console.log("Saturnday");
+        break; 
+}
